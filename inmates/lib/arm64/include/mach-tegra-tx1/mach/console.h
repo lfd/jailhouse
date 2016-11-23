728cb746e0ce5327a099248de8cb1edd83171a7e
Claudio Scordino <claudio@evidence.eu.com>
2016-11-23 09:49:20 +0100

    inmates: add support for Tegra TX1
    This patch adds the inmate support for the Tegra TX1 board.
    
    Signed-off-by: Claudio Scordino <claudio@evidence.eu.com>
    [ralf: rename uart.h to console.h and align console.h]
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

---
+++ inmates/lib/arm64/include/mach-tegra-tx1/mach/console.h
@@ -0,0 +1,15 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) Siemens AG, 2015
+ *
+ * Authors:
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#define CON_TYPE	"8250"
+
+#define CON_BASE	0x70006000
