bcbe8cabb0da9d3db93a5bbba8cbd9a782aaf71d
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2017-09-11 16:01:26 +0200

    inmates: arm-common: introduce processor.h
    cpu_relax() is currently defined in inmate.h. But we need cpu_relax() in
    places where we don't want to include inmate.h. Use this chance to
    implement memory_barrier() as well.
    
    As ARM and ARM64 don't differ for those definitions we can use a common
    header file for both architectures.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- inmates/lib/arm-common/include/asm/processor.h
+++ inmates/lib/arm-common/include/asm/processor.h
@@ -1,10 +1,10 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Copyright (c) ARM Limited, 2014
+ * Copyright (c) OTH Regensburg, 2017
  *
  * Authors:
- *  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
@@ -36,60 +36,14 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef _JAILHOUSE_INMATE_H
-#define _JAILHOUSE_INMATE_H
+#define dmb(domain)	asm volatile("dmb " #domain ::: "memory")
 
-typedef signed char s8;
-typedef unsigned char u8;
-
-typedef signed short s16;
-typedef unsigned short u16;
-
-typedef signed int s32;
-typedef unsigned int u32;
-
-typedef signed long long s64;
-typedef unsigned long long u64;
-
-static inline u8 mmio_read8(void *address)
+static inline void cpu_relax(void)
 {
-	return *(volatile u8 *)address;
+	asm volatile("" : : : "memory");
 }
 
-static inline void mmio_write8(void *address, u8 value)
+static inline void memory_barrier(void)
 {
-	*(volatile u8 *)address = value;
+	dmb(ish);
 }
-
-static inline void mmio_write16(void *address, u16 value)
-{
-	*(volatile u16 *)address = value;
-}
-
-static inline u32 mmio_read32(void *address)
-{
-	return *(volatile u32 *)address;
-}
-
-static inline void mmio_write32(void *address, u32 value)
-{
-	*(volatile u32 *)address = value;
-}
-
-typedef void (*irq_handler_t)(unsigned int);
-void gic_setup(irq_handler_t handler, void *irq_stack);
-void gic_enable_irq(unsigned int irq);
-
-unsigned long timer_get_frequency(void);
-u64 timer_get_ticks(void);
-u64 timer_ticks_to_ns(u64 ticks);
-void timer_start(u64 timeout);
-
-void inmate_main(void *irq_stack);
-
-#include <asm/processor.h>
-#include <arch/inmate.h>
-
-#include "../inmate_common.h"
-
-#endif /* !_JAILHOUSE_INMATE_H */
