lfd/jailhouse

commit 95202a038b9eddd3662a0e82c70b8108a42f1863

x86,mmio: Add support for 1 more instructions on the MMIO dispatcher
Now comes

X86_OP_MOV_IMMEDIATE_TO_MEM                     0xc7,

a instruction with an immediate value encoded in. The reason to bring it
in is that the code is simple enough and will enable one more guest
OS (Zephyr OS) to function in xAPIC mode as well.

Because we now have to keep state from a contiguous memory region (the
immediate value) and an instruction may fall in between two pages in the
guest, there is extra code handling that.

Signed-off-by: Gustavo Lima Chaves <gustavo.lima.chaves@intel.com>
Signed-off-by: Otavio Pontes <otavio.pontes@intel.com>
[Jan: declare loop counter earlier and as unsigned]
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/mmio.c
@@ -21,6 +21,13 @@
 
 #define X86_MAX_INST_LEN	15
 
+/*
+ * There are a few instructions that can have 8-byte immediate values
+ * on 64-bit mode, but they are not supported/expected here, so we are
+ * safe.
+ */
+#define IMMEDIATE_SIZE          4
+
 union opcode {
 	u8 raw;
 	struct { /* REX */
@@ -72,10 +79,12 @@ struct mmio_instruction x86_mmio_parse(unsigned long pc,
 				     .count = 1 };
 	union registers *guest_regs = &this_cpu_data()->guest_regs;
 	struct mmio_instruction inst = { .inst_len = 0 };
+	bool has_immediate = false;
 	union opcode op[4] = { };
 	bool does_write = false;
 	bool has_rex_w = false;
 	bool has_rex_r = false;
+	unsigned int n;
 
 	if (!ctx_update(&ctx, &pc, 0, pg_structs))
 		goto error_noinst;
@@ -117,6 +126,11 @@ restart:
 	case X86_OP_MOV_FROM_MEM:
 		inst.access_size = has_rex_w ? 8 : 4;
 		break;
+	case X86_OP_MOV_IMMEDIATE_TO_MEM:
+		inst.access_size = has_rex_w ? 8 : 4;
+		has_immediate = true;
+		does_write = true;
+		break;
 	case X86_OP_MOV_MEM_TO_AX:
 		inst.inst_len = ctx.count + 4;
 		inst.access_size = has_rex_w ? 8 : 4;
@@ -140,6 +154,10 @@ restart:
 	case 0:
 		if (op[2].modrm.rm == 5) { /* 32-bit displacement */
 			inst.inst_len += 4;
+			/* walk displacement bytes, to point to immediate */
+			if (has_immediate &&
+			    !ctx_update(&ctx, &pc, 4, pg_structs))
+				goto error_noinst;
 			break;
 		} else if (op[2].modrm.rm != 4) { /* no SIB */
 			break;
@@ -171,7 +189,13 @@ restart:
 	else
 		inst.in_reg_num = 15 - op[2].modrm.reg;
 
-	if (does_write)
+	if (has_immediate)
+		for (n = 0; n < IMMEDIATE_SIZE; n++) {
+			if (!ctx_update(&ctx, &pc, 1, pg_structs))
+				goto error_noinst;
+			inst.out_val |= (unsigned long)*ctx.inst << (n * 8);
+		}
+	else if (does_write)
 		inst.out_val = guest_regs->by_index[inst.in_reg_num];
 
 final:

hypervisor/arch/x86/include/asm/processor.h
@@ -149,6 +149,7 @@
 #define X86_OP_MOVB_TO_MEM				0x88
 #define X86_OP_MOV_TO_MEM				0x89
 #define X86_OP_MOV_FROM_MEM				0x8b
+#define X86_OP_MOV_IMMEDIATE_TO_MEM			0xc7
 #define X86_OP_MOV_MEM_TO_AX    			0xa1
 #define X86_OP_MOV_AX_TO_MEM				0xa3
 

commit adbcf414b6705353eeafe553622d5da0f374935c

x86: mmio: Fix instruction length calculation for write-immediate
This fixes one case around mov imm,<addr>: We read the immediate value
to be written, but we forgot to update inst_len. This fixes that issue
by moving the update from ctx.count to inst.inst_len to the very end
of the parser.

Fixes: 95202a038b9e ("x86,mmio: Add support for 1 more instructions on
                      the MMIO dispatcher")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/mmio.c
@@ -1,7 +1,7 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Copyright (c) Siemens AG, 2013
+ * Copyright (c) Siemens AG, 2013-2018
  * Copyright (c) Valentine Sinitsyn, 2014
  *
  * Authors:
@@ -130,12 +130,12 @@ restart:
 		does_write = true;
 		break;
 	case X86_OP_MOV_MEM_TO_AX:
-		inst.inst_len = ctx.count + 4;
+		inst.inst_len += 4;
 		inst.access_size = has_rex_w ? 8 : 4;
 		inst.in_reg_num = 15;
 		goto final;
 	case X86_OP_MOV_AX_TO_MEM:
-		inst.inst_len = ctx.count + 4;
+		inst.inst_len += 4;
 		inst.access_size = has_rex_w ? 8 : 4;
 		inst.out_val = guest_regs->by_index[15];
 		does_write = true;
@@ -179,7 +179,6 @@ restart:
 		goto error_unsupported;
 	}
 
-	inst.inst_len += ctx.count;
 	if (has_rex_r)
 		inst.in_reg_num = 7 - op[2].modrm.reg;
 	else if (op[2].modrm.reg == 4)
@@ -199,6 +198,8 @@ restart:
 final:
 	if (does_write != is_write)
 		goto error_inconsitent;
+
+	inst.inst_len += ctx.count;
 
 	return inst;
 