lfd/jailhouse

commit ea43ce86f4e31d87b00bafa978ad88dda2b8ca95

x86,mmio: Add support for 2 more instructions on the MMIO dispatcher
Those are:

X86_OP_MOV_MEM_TO_AX                           0xa1
X86_OP_MOV_AX_TO_MEM                           0xa3

The reason to bring them in is that the code is simple enough and will
enable one more guest OS (Zephyr OS) to function in xAPIC mode as well.

Signed-off-by: Gustavo Lima Chaves <gustavo.lima.chaves@intel.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/mmio.c
@@ -119,6 +119,17 @@ restart:
 		inst.inst_len += 2;
 		inst.access_size = has_rex_w ? 8 : 4;
 		break;
+	case X86_OP_MOV_MEM_TO_AX:
+		inst.inst_len += 5;
+		inst.access_size = has_rex_w ? 8 : 4;
+		inst.reg_num = 15;
+		goto final;
+	case X86_OP_MOV_AX_TO_MEM:
+		inst.inst_len += 5;
+		inst.access_size = has_rex_w ? 8 : 4;
+		inst.reg_num = 15;
+		does_write = true;
+		goto final;
 	default:
 		goto error_unsupported;
 	}
@@ -161,6 +172,7 @@ restart:
 	else
 		inst.reg_num = 15 - op[2].modrm.reg;
 
+final:
 	if (does_write != is_write)
 		goto error_inconsitent;
 

hypervisor/arch/x86/include/asm/processor.h
@@ -149,6 +149,8 @@
 #define X86_OP_MOVB_TO_MEM				0x88
 #define X86_OP_MOV_TO_MEM				0x89
 #define X86_OP_MOV_FROM_MEM				0x8b
+#define X86_OP_MOV_MEM_TO_AX    			0xa1
+#define X86_OP_MOV_AX_TO_MEM				0xa3
 
 #define DB_VECTOR					1
 #define NMI_VECTOR					2

commit 99136353dac8363e570c96c3eaafa60247c14bee

x86: mmio: Fix parsing for mov ax to/from mem in 64-bit mode
In long mode, we have 64-bit addresses by default, and so we need to
skip over 8 bytes of address for that instruction.

Fixes: ea43ce86f4e3 ("x86,mmio: Add support for 2 more instructions on
                      the MMIO dispatcher")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/mmio.c
@@ -75,6 +75,7 @@ x86_mmio_parse(const struct guest_paging_structures *pg_structs, bool is_write)
 	struct parse_context ctx = { .remaining = X86_MAX_INST_LEN,
 				     .count = 1 };
 	union registers *guest_regs = &this_cpu_data()->guest_regs;
+	bool addr64 = !!(vcpu_vendor_get_efer() & EFER_LMA);
 	struct mmio_instruction inst = { .inst_len = 0 };
 	u64 pc = vcpu_vendor_get_rip();
 	unsigned int n, skip_len = 0;
@@ -130,12 +131,12 @@ restart:
 		does_write = true;
 		break;
 	case X86_OP_MOV_MEM_TO_AX:
-		inst.inst_len += 4;
+		inst.inst_len += addr64 ? 8 : 4;
 		inst.access_size = has_rex_w ? 8 : 4;
 		inst.in_reg_num = 15;
 		goto final;
 	case X86_OP_MOV_AX_TO_MEM:
-		inst.inst_len += 4;
+		inst.inst_len += addr64 ? 8 : 4;
 		inst.access_size = has_rex_w ? 8 : 4;
 		inst.out_val = guest_regs->by_index[15];
 		does_write = true;