2535ebf47750cb76e5952d7dfba8f4bb6c38e13b
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2018-06-01 02:05:07 +0200

    inmates: introduce printk.h
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- inmates/lib/include/printk.h
+++ inmates/lib/include/printk.h
@@ -1,10 +1,10 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Copyright (c) Siemens AG, 2013-2016
+ * Copyright (c) Ralf Ramsauer, 2018
  *
  * Authors:
- *  Jan Kiszka <jan.kiszka@siemens.com>
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
@@ -36,26 +36,4 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define NS_PER_USEC		1000UL
-#define NS_PER_MSEC		1000000UL
-#define NS_PER_SEC		1000000000UL
-
-#ifndef __ASSEMBLY__
-
-#include <jailhouse/hypercall.h>
-
-void arch_init_early(void);
-
-void inmate_main(void);
-
-#include <alloc.h>
-#include <cmdline.h>
-#include <layout.h>
-#include <mem.h>
-#include <mmio.h>
-#include <printk.h>
-#include <string.h>
-
-#include <asm/processor.h>
-
-#endif /* !__ASSEMBLY__ */
+void __attribute__((format(printf, 1, 2))) printk(const char *fmt, ...);
