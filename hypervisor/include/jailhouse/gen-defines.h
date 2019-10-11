1c895a44b92571a2a29dcf9f58c438a75b995e59
Andrej Utz <andrej.utz@st.oth-regensburg.de>
2019-10-11 16:27:19 +0200

    core: Fix ASM define generation with Clang compiler
    Apply changes from Linux kernel.
    See kernel merge commit 593043d35ddf for more details.
    
    Signed-off-by: Andrej Utz <andrej.utz@st.oth-regensburg.de>

--- hypervisor/include/jailhouse/gen-defines.h
+++ hypervisor/include/jailhouse/gen-defines.h
@@ -11,21 +11,21 @@
  *
  * Derived from linux/include/linux/kbuild.h:
  *
- * Copyright (c) Linux kernel developers, 2014
+ * Copyright (c) Linux kernel developers, 2014,2017
  */
 
 #ifndef _JAILHOUSE_GEN_DEFINES_H
 #define _JAILHOUSE_GEN_DEFINES_H
 
 #define DEFINE(sym, val) \
-        asm volatile("\n=>" #sym " %0 " #val : : "i" (val))
+	asm volatile("\n.ascii \"->" #sym " %0 " #val "\"" : : "i" (val))
 
-#define BLANK() asm volatile("\n=>" : : )
+#define BLANK() asm volatile("\n.ascii \"->\"" : : )
 
 #define OFFSET(sym, str, mem) \
 	DEFINE(sym, __builtin_offsetof(struct str, mem))
 
 #define COMMENT(x) \
-	asm volatile("\n=>#" x)
+	asm volatile("\n.ascii \"->#" x "\"")
 
 #endif
