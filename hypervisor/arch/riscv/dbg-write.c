402ccf1f01cfcf160442f499e633d7f036060edc
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2022-01-04 23:44:20 +0100

    core: riscv: add dbg-write helper
    For the moment, only support the fast&easy output path that is always
    available: SBI. No UARTs needed for the hypervisor, SBI suffices atm.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- hypervisor/arch/riscv/dbg-write.c
+++ hypervisor/arch/riscv/dbg-write.c
@@ -4,14 +4,24 @@
  * Copyright (c) Siemens AG, 2020
  *
  * Authors:
- *  Jan Kiszka <jan.kiszka@siemens.com>
+ *  Konrad Schwarz <konrad.schwarz@siemens.com>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
  */
 
 #include <jailhouse/printk.h>
+#include <asm/sbi.h>
 
-void arch_dbg_write_init(void)
+static void riscv_dbg_write_sbi(const char *msg)
 {
+	char ch;
+
+	while ((ch = *msg++))
+		sbi_console_putchar_legacy0_1(ch);
+}
+
+void arch_dbg_write_init (void)
+{
+        arch_dbg_write = riscv_dbg_write_sbi;
 }
