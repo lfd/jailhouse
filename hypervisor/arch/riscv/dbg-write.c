1ce16d830bda689db3f5e09e9eafc49062d915c2
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2022-01-04 23:44:20 +0100

    core: riscv: add dbg-write helper
    The easiest on RISC-V is to use the SBI console.
    
    However, there may be setups that require the hypervisor console on a
    secondary UART. Add support for the 8250 uart for now.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
    [since v3: add support for UARTs beside SBI]

--- hypervisor/arch/riscv/dbg-write.c
+++ hypervisor/arch/riscv/dbg-write.c
@@ -4,14 +4,40 @@
  * Copyright (c) Siemens AG, 2020
  *
  * Authors:
- *  Jan Kiszka <jan.kiszka@siemens.com>
+ *  Konrad Schwarz <konrad.schwarz@siemens.com>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
  */
 
+#include <asm/sbi.h>
+#include <jailhouse/types.h>
+#include <jailhouse/console.h>
+#include <jailhouse/control.h>
 #include <jailhouse/printk.h>
+#include <jailhouse/uart.h>
+
+static void riscv_dbg_write_sbi(const char *msg)
+{
+	char ch;
+
+	while ((ch = *msg++))
+		sbi_console_putchar_legacy0_1(ch);
+}
 
 void arch_dbg_write_init(void)
 {
+	unsigned int con_type = system_config->debug_console.type;
+
+	if (con_type == JAILHOUSE_CON_TYPE_RISCV_SBI)
+		arch_dbg_write = riscv_dbg_write_sbi;
+        else if (con_type == JAILHOUSE_CON_TYPE_8250)
+                uart = &uart_8250_ops;
+
+	if (uart) {
+		uart->debug_console = &system_config->debug_console;
+		uart->virt_base = hypervisor_header.debug_console_base;
+		uart->init(uart);
+		arch_dbg_write = uart_write;
+	}
 }
