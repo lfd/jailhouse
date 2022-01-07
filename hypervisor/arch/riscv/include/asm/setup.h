e71b40907dc884b15595852f8649c76ec9611600
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2022-01-07 21:21:50 +0100

    core: riscv: Hypervisor entry code
    Now we have to think of Jailhouse's memory layout.
    
    RISC-V has a bunch of MMU variants SVxx (SV39, SV48, SV57), while xx
    denotes the number of virtually adressable bits. And there's an
    exceptional case: If bit xx-1 is set in a vaddr, then all bits >= XX
    must be 1.
    
    For the virtual HV location, we need a virtual address that is valid for
    each MMU type. Let's virtually place the hypervisor at the topmost 16
    MiB of a SV39. This results in address 0xffff_ffd0_0000_0000.
    
    On RISC-V, Linux could map the hypervisor to any virtual location, so we
    need to setup bootstrap page tables that help us to jump to our link
    location. Now this gets funny, as we need to support ALL MMU types for
    the trampoline, as Linux might map us to a very high address.
    
    The position-independent startup/trampoline code sets up a trampoline
    that consists of two entries:
      - Virtual Linux Load Location -> physical HV memory
      - 0xffff_ffd0_0000_0000       -> physical HV memory
    
    The startup code will create and activate this table, and jump to the
    destination address where it was linked. From now on, we can use
    position-dependant code.
    
    Later, those trampoline page tables will not be destroyed, as we can
    reuse them when disabling the hypervisor, or when errors occur during
    startup.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
    [since v3: add mapping for debug console]

---
+++ hypervisor/arch/riscv/include/asm/setup.h
@@ -0,0 +1,14 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) OTH Regensburg, 2022-2023
+ *
+ * Authors:
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+void __attribute__((noreturn))
+riscv_deactivate_vmm(union registers *regs, int errcode, bool from_ecall);
