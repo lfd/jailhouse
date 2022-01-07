0349402c3207483fff0d8b64dddbc1132a4e57dd
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2022-01-07 21:21:50 +0100

    core: riscv: Hypervisor entry code
    Now we have to think of Jailhouse's memory layout.
    
    RISC-V has a bunch of MMU variants SVxx (SV39, SV48, SV57), while xx
    denotes the number of virtually adressable bits. And there's an
    exceptional case: If bit xx-1 is set in a vaddr, then all bits >= XX
    must be 1.
    
    For the virtual HV location, we need a virtual address that is valid for
    each MMU type. Let's virtually place the hypervisor at the topmost 16
    MiB of a SV39. This results in address 0xffff_ffd0_0000_0000.
    
    On RISC-V, Linux could map the hypervisor to any virtual location, so we
    need to setup bootstrap page tables that help us to jump to our link
    location. Now this gets funny, as we need to support ALL MMU types for
    the trampoline, as Linux might map us to a very high address.
    
    The position-independent startup/trampoline code sets up a trampoline
    that consists of two entries:
      - Virtual Linux Load Location -> physical HV memory
      - 0xffff_ffd0_0000_0000       -> physical HV memory
    
    The startup code will create and activate this table, and jump to the
    destination address where it was linked. From now on, we can use
    position-dependant code.
    
    Later, those trampoline page tables will not be destroyed, as we can
    reuse them when disabling the hypervisor, or when errors occur during
    startup.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- hypervisor/arch/riscv/include/asm/jailhouse_header.h
+++ hypervisor/arch/riscv/include/asm/jailhouse_header.h
@@ -4,11 +4,29 @@
  * Copyright (C) Siemens AG, 2020
  *
  * Authors:
- *  Jan Kiszka <jan.kiszka@siemens.com>
+ *  Konrad Schwarz <konrad.schwarz@siemens.com>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
  */
 
-#define JAILHOUSE_BASE			__JH_CONST_UL(0xffffffdfff000000)
-#define JAILHOUSE_BORROW_ROOT_PT	1
+/*
+ * RISC-V defined architectural constraints on the address space:
+ * Address immediates are sign extended.
+ *
+ * lui+jalr can jump to absolute addresses +-2 GiB around zero
+ *  --> this best left free for millicode routines (or boot ROM),
+ *      at least on 64-bit machines.
+ *      This is the range 0xffff_ffff_8000_000--0x0000_0000_7fff_ffff
+ *
+ * Legal virtual addresses are sign extensions of the most-significant bit
+ * mapped by the virtual addressing mode; e.g., in Sv39, bits 63:39 of an
+ * address must be a copy of bit 38. Hence, the lowest (signed) address
+ * available is 0xffff_ffc0_0000_0000.
+ *
+ * Linux normally maps itself at 0xffff_ffe0_0000_0000 and modules at
+ * 0xffff_ffd0_0000_0000.
+ */
+
+/* this provides 16 MB of space for the Jailhouse core */
+#define JAILHOUSE_BASE			0xffffffdfff000000
