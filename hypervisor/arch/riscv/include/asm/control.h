c1f936d84e4813a1e9c583ca0cc42ffdada8dd4f
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2022-01-08 16:44:47 +0100

    core: riscv: implement control.c
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
    [RFC v2: fix incorrect hsm state on cell reset]

---
+++ hypervisor/arch/riscv/include/asm/control.h
@@ -0,0 +1,13 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) OTH Regensburg, 2022
+ *
+ * Authors:
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+void riscv_park_cpu(void);
