a1f9de66501c12346604bf348c402c1e3ba14d1e
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2022-05-18 15:17:45 +0200

    core: riscv: add asm-defines.c
    Export offsets that we will later need in assembly.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
    [since v3: export more offsets]

--- hypervisor/arch/riscv/asm-defines.c
+++ hypervisor/arch/riscv/asm-defines.c
@@ -11,9 +11,43 @@
  */
 
 #include <jailhouse/gen-defines.h>
+#include <jailhouse/percpu.h>
 
 void common(void);
 
 void common(void)
 {
+	OFFSETU(REG_SP, registers, sp);
+	OFFSETU(REG_RA, registers, ra);
+	OFFSETU(REG_T0, registers, t0);
+	OFFSETU(REG_TP, registers, tp);
+	OFFSETU(REG_GP, registers, gp);
+	OFFSETU(REG_S0, registers, s0);
+	OFFSETU(REG_S1, registers, s1);
+	OFFSETU(REG_S2, registers, s2);
+	OFFSETU(REG_S3, registers, s3);
+	OFFSETU(REG_S4, registers, s4);
+	OFFSETU(REG_S5, registers, s5);
+	OFFSETU(REG_S6, registers, s6);
+	OFFSETU(REG_S7, registers, s7);
+	OFFSETU(REG_S8, registers, s8);
+	OFFSETU(REG_S9, registers, s9);
+	OFFSETU(REG_S10, registers, s10);
+	OFFSETU(REG_S11, registers, s11);
+
+	OFFSET(HEADER_MAX_CPUS, jailhouse_header, max_cpus);
+	OFFSET(HEADER_PERCPU_SIZE, jailhouse_header, percpu_size);
+	OFFSET(HEADER_DEBUG_CONSOLE_VIRT, jailhouse_header, debug_console_base);
+
+	OFFSET(SYSCONFIG_HYPERVISOR_PHYS, jailhouse_system,
+	       hypervisor_memory.phys_start);
+	OFFSET(SYSCONFIG_DEBUG_CONSOLE_PHYS, jailhouse_system,
+	       debug_console.address);
+	OFFSET(SYSCONFIG_DEBUG_CONSOLE_TYPE, jailhouse_system,
+	       debug_console.type);
+
+
+	OFFSET(PCPU_GUEST_REGS, per_cpu, guest_regs);
+
+	DEFINE(REGISTERS_SIZE, sizeof(union registers));
 }
