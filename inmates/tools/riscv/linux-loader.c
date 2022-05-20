6d80dc7cf7af0cd18aa5a300f4cbbe538a3c3bdd
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2022-05-20 18:15:03 +0200

    inmates: riscv: implement Linux Loader
    The boot protocol is simple:
     - a0 hold the hart id
     - a1 hold the DTB
    
    That's it.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

---
+++ inmates/tools/riscv/linux-loader.c
@@ -0,0 +1,25 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) OTH Regensburg, 2022
+ *
+ * Authors:
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *  Stefan Huber <stefan.huber@oth-regensburg.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <inmate.h>
+
+void inmate_main(void)
+{
+	void (*entry)(u64 hartid, u64 dtb);
+	unsigned long dtb;
+
+	entry = (void *)cmdline_parse_int("kernel", 0);
+	dtb = cmdline_parse_int("dtb", 0);
+
+	entry(hart_id, dtb);
+}
