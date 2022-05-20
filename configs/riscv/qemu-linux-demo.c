a92e9f4dd8c0195878d56483fdb1440f84bd5e99
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2022-05-20 14:44:13 +0200

    configs: riscv: qemu: Add linux-demo
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
    [RFC v2: fix typo]

---
+++ configs/riscv/qemu-linux-demo.c
@@ -0,0 +1,102 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Configuration for RISC-V Linux inmate on QEMU
+ *
+ * Copyright (c) OTH Regensburg 2022
+ *
+ * Authors:
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/types.h>
+#include <jailhouse/cell-config.h>
+
+struct {
+	struct jailhouse_cell_desc cell;
+	struct jailhouse_cpu cpus[2];
+	struct jailhouse_memory mem_regions[8];
+	struct jailhouse_irqchip irqchips[1];
+	struct jailhouse_pci_device pci_devices[0];
+} __attribute__((packed)) config = {
+	.cell = {
+		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
+		.revision = JAILHOUSE_CONFIG_REVISION,
+		.architecture = JAILHOUSE_RISCV64,
+		.name = "linux-demo",
+		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG |
+			JAILHOUSE_CELL_TEST_DEVICE |
+			JAILHOUSE_CELL_VIRTUAL_CONSOLE_PERMITTED,
+
+		.num_cpus = ARRAY_SIZE(config.cpus),
+		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+		.num_irqchips = ARRAY_SIZE(config.irqchips),
+		.num_pci_devices = ARRAY_SIZE(config.pci_devices),
+
+		.console = {
+			.type = JAILHOUSE_CON_TYPE_8250,
+			.size = 0x1000,
+			.address = 0x10000000,
+			.flags = JAILHOUSE_CON_ACCESS_MMIO | JAILHOUSE_CON_REGDIST_1,
+		},
+
+		.vpci_irq_base = 32,
+	},
+
+	.cpus = {
+		{
+			.phys_id = 1,
+		},
+		{
+			.phys_id = 2,
+		},
+	},
+
+	.mem_regions = {
+		/* RAM low */ {
+			.phys_start = 0xbf800000,
+			.virt_start = 0x0,
+			.size = 1 * 1024 * 1024,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+		},
+		/* communication region */ {
+			.virt_start = 0x00100000,
+			.size = 0x00001000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_COMM_REGION,
+		},
+		/* RAM high */ {
+			.phys_start = 0xbb800000,
+			.virt_start = 0x80000000,
+			.size = 64 * 1024 * 1024,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+		},
+		/* uart@10000000 */ {
+			.phys_start = 0x10000000,
+			.virt_start = 0x10000000,
+			.size = 0x1000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_ROOTSHARED,
+		},
+		/* IVSHMEM shared memory regions (networking) */
+		JAILHOUSE_SHMEM_NET_REGIONS(0xbf900000, 1),
+	},
+
+	.pci_devices = {
+	},
+
+	.irqchips = {
+		/* PLIC */ {
+			.address = 0xc000000,
+			.pin_base = 0,
+			.pin_bitmap = {
+				(1 << 0xa), 0, 0, 0,
+			},
+		},
+	},
+};
