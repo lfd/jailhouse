c04b662a89003132dc1e7360f6f39e56f395ef08
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2024-12-17 16:35:28 +0100

    cell: activate aarch32

--- configs/arm64/qemu-arm64-inmate-demo.c
+++ configs/arm64/qemu-arm64-inmate-demo.c
@@ -28,7 +28,7 @@ struct {
 		.revision = JAILHOUSE_CONFIG_REVISION,
 		.architecture = JAILHOUSE_ARM64,
 		.name = "inmate-demo",
-		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
+		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG | JAILHOUSE_CELL_AARCH32,
 
 		.cpu_set_size = sizeof(config.cpus),
 		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
